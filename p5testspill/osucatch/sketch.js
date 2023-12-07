d891c34efe754a05369a350700fb7e55e80b967f
Ulrik Bolling Nesheim <ulrik@dethvitehus.org>
2023-12-07 23:22:27 +0100

    * feilretting slik at spillet stopper når man får 0 liv * brukerveiledning innebygd i spillet

--- p5testspill/osucatch/sketch.js
+++ p5testspill/osucatch/sketch.js
@@ -9,7 +9,13 @@ let score = 0;
 let controls;
 let controls_button;
 let control_text;
+let info;
+let info_button;
+let info_text;
 let show_controlls = false;
+let show_info = false;
+
+let playing = false;
 
 function setup() {
 	new Canvas(windowWidth, windowHeight);
@@ -18,7 +24,7 @@ function setup() {
 	avoiders = new Sprite;
 	avoiders.width = 75;
 	avoiders.height = 50;
-	avoiders.x = random(350, width - 350);
+	avoiders.x = width / 2;
 	avoiders.y = 30;
 	avoiders.color = 'red';
 
@@ -29,7 +35,7 @@ function setup() {
 
 	//floor
 	floor = new Sprite();
-	floor.y = height-20	;
+	floor.y = height - 20;
 	floor.w = width;
 	floor.h = 3;
 	floor.color = 'red';
@@ -88,13 +94,63 @@ function setup() {
 	controls_button.width = 150;
 	controls_button.height = 50;
 	controls_button.collider = 'null';
+
+	info = new Sprite;
+	info.img = '../../spillside/images/info.png';
+	info.x = width - width / 8;
+	info.y = height / 2 + 70;
+	info.width = 30;
+	info.height = 30;
+	info.collider = 'none';
+
+	info_button = new Sprite;
+	info_button.text = "Info";
+	info_button.color = 'lightblue';
+	info_button.textSize = 30;
+	info_button.x = width - width / 8;
+	info_button.y = height / 2 + 140;
+	info_button.width = 150;
+	info_button.height = 50;
+	info_button.collider = 'none';
+
+	startgame = new Sprite;
+	startgame.text = "Play game";
+	startgame.textSize = 30;
+	startgame.x = width / 2;
+	startgame.y = height / 2;
+	startgame.width = 200;
+	startgame.height = 100;
+	startgame.layer = 2;
+	startgame.color = 'green';
+	startgame.collider = 'null';
+
 }
 
 
 function draw() {
 	background('gray');
 
-	if (mouse.presses() && mouseX > controls_button.x - 50 && mouseX < controls_button.x + 50 && mouseY > controls_button.y - 20 && mouseY < controls_button.y + 20) {
+	if (startgame && playing == false && mouse.presses() && mouseX > startgame.x - 75 && mouseX < startgame.x + 75 && mouseY > startgame.y - 40 && mouseY < startgame.y + 40) {
+		playing = true;
+		avoiders.x = width / 2;
+		player.x = width / 2;
+
+		startgame.remove();
+		info.remove();
+		info_button.remove();
+		controls.remove();
+		controls_button.remove();
+		if (show_controlls == true) {
+			info_text.remove();
+		}
+		if (show_controlls == true) {
+			control_text.remove();
+		}
+
+
+	}
+
+	if (mouse.presses() && mouseX > controls_button.x - 75 && mouseX < controls_button.x + 75 && mouseY > controls_button.y - 25 && mouseY < controls_button.y + 25) {
 		if (show_controlls == true) {
 			control_text.remove();
 			show_controlls = false;
@@ -108,124 +164,199 @@ function draw() {
 			control_text.width = width / 5;
 			control_text.height = height / 5;
 			control_text.collider = 'null';
-			//left aligne virker ikke
-			//			control_text.textAlign = LEFT; 
 			show_controlls = true;
 		}
 	}
 
+
+	if (mouse.presses() && mouseX > info_button.x - 75 && mouseX < info_button.x + 75 && mouseY > info_button.y - 25 && mouseY < info_button.y + 25) {
+		if (show_info == true) {
+			info_text.remove();
+			show_info = false;
+		} else {
+			info_text = new Sprite;
+			info_text.text = "CATCHE'M ALL info: \n ta imot de fallene sirklene, \n for å få poeng, hvis noen faller \n i bakken vil du miste ett liv";
+			info_text.color = 'lightgray';
+			info_text.textSize = 24;
+			info_text.x = width - width / 8;
+			info_text.y = height / 2 + 250;
+			info_text.width = width / 5 + 20;
+			info_text.height = 150;
+			info_text.collider = 'none';
+
+			show_info = true;
+		}
+	}
+
+
 	if (kb.pressing('left')) {
 		if (kb.pressing('shift')) {
 			player.vel.x = -15;
-		} else { 
-		player.vel.x = -7;
+		} else {
+			player.vel.x = -7;
 		}
 	}
 	else if (kb.pressing('right')) {
 		if (kb.pressing('shift')) {
 			player.vel.x = 15;
-		} else { 
-		player.vel.x = 7;
-	}
-	}	else {
+		} else {
+			player.vel.x = 7;
+		}
+	} else {
 		player.vel.x = 0;
 	}
 
-	if (health <= 0) {
-		if(score > 0){ 
-		let xhr = new XMLHttpRequest();
+	if (health <= 0 && playing == true) {
+		if (score > 0) {
+			let xhr = new XMLHttpRequest();
 
-		// 2. Configure it: GET-request for the URL /article/.../load
-		xhr.open('GET', "../../spillside/includes/scorehandler.inc.php?spill=CATCHE'M ALL&poeng="+score);
+			// 2. Configure it: GET-request for the URL /article/.../load
+			xhr.open('GET', "../../spillside/includes/scorehandler.inc.php?spill=CATCHE'M ALL&poeng=" + score);
 
-		// 3. Send the request over the network
-		xhr.send();
+			// 3. Send the request over the network
+			xhr.send();
 
-		xhr.onload = function() {
-			if (xhr.status != 200) { // analyze HTTP status of the response
-			  alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
-			} 
-		  };
+			xhr.onload = function () {
+				if (xhr.status != 200) { // analyze HTTP status of the response
+					alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+				}
+			};
 
 		}
 		score = 0;
 		health = 25;
+		playing = false
+		notPlaying();
 	}
-
 	fill(0);
 	textSize(40);
-	text(`Score: ${score}`, 10, 100);	
-	text(`health: ${health}`, 10, 180);	
+	text(`Score: ${score}`, 10, 100);
+	text(`health: ${health}`, 10, 180);
 	health = constrain(health, 0, 100); // Ensure health stays within 0 to 100
 	drawHealthBar(10, 200, 200, 20, health, 25);
 }
 
 setInterval(() => {
-	ball = new Sprite();
-	ball.diameter = Math.floor(Math.random() * ((25 - 8) +1) + 10)
-	ball.x = random((avoiders.x - 40), (avoiders.x + 40));
-	ball.y = random(avoiders.y + 40, avoiders.y + 60);
-	// gems.amount = 2;
+	if (playing == true) {
+		ball = new Sprite();
+		ball.diameter = Math.floor(Math.random() * ((25 - 8) + 1) + 10)
+		ball.x = random((avoiders.x - 40), (avoiders.x + 40));
+		ball.y = random(avoiders.y + 40, avoiders.y + 60);
+		// gems.amount = 2;
 
-	ball.collide(player, collect);
-	ball.collide(floor, healthbar);
+		ball.collide(player, collect);
+		ball.collide(floor, healthbar);
+	}
 
 }, 100);
 
 setInterval(() => {
-	if (avoiders.x > width - 200) {
-		avoiders.speed = random(-8, -15);
-	} else if (avoiders.x < 200) {
-		avoiders.speed = random(8, 15);
-	} else {
-		avoiders.speed = random(random(8, 12), random(-8, -12));
+	if (playing == true) {
+		if (avoiders.x > width - 200) {
+			avoiders.speed = random(-8, -15);
+		} else if (avoiders.x < 200) {
+			avoiders.speed = random(8, 15);
+		} else {
+			avoiders.speed = random(random(8, 12), random(-8, -12));
+		}
 	}
 }, 1000);
 
+function notPlaying() {
+	startgame = new Sprite;
+	startgame.text = "Play game";
+	startgame.textSize = 30;
+	startgame.x = width / 2;
+	startgame.y = height / 2;
+	startgame.width = 200;
+	startgame.height = 100;
+	startgame.layer = 2;
+	startgame.color = 'green';
+	startgame.collider = 'null';
+
+
+	controls = new Sprite;
+	controls.img = '../../spillside/images/controller.png';
+	controls.x = width - width / 8;
+	controls.y = 110;
+	controls.width = 30;
+	controls.height = 30;
+	controls.collider = 'null';
+
+	controls_button = new Sprite;
+	controls_button.text = "Controlls";
+	controls_button.color = 'lightblue';
+	controls_button.textSize = 30;
+	controls_button.x = width - width / 8;
+	controls_button.y = 180;
+	controls_button.width = 150;
+	controls_button.height = 50;
+	controls_button.collider = 'null';
+
+	info = new Sprite;
+	info.img = '../../spillside/images/info.png';
+	info.x = width - width / 8;
+	info.y = height / 2 + 70;
+	info.width = 30;
+	info.height = 30;
+	info.collider = 'none';
+
+	info_button = new Sprite;
+	info_button.text = "Info";
+	info_button.color = 'lightblue';
+	info_button.textSize = 30;
+	info_button.x = width - width / 8;
+	info_button.y = height / 2 + 140;
+	info_button.width = 150;
+	info_button.height = 50;
+	info_button.collider = 'none';
+}
 
 function drawHealthBar(x, y, width, height, current, max) {
 	// Calculate the width of the green (health) bar
 	let healthBarWidth = (current / max) * width;
-  
+
 	// Draw the red (missing health) bar
 	fill(255, 0, 0);
 	rect(x, y, width, height);
-  
+
 	// Draw the green (health) bar
 	fill(0, 255, 0);
 	rect(x, y, healthBarWidth, height);
-  }
-  
+}
 
-  function collect(ball, player) {
-	let pointText = "+1 point";
+
+function collect(ball, player) {
+	if (playing == true) {
+		let pointText = "+1 point";
+		let textX = ball.x;
+		let textY = ball.y - 20; // Display the text just above the ball's position
+
+		fill('green');
+		text(pointText, textX, textY); // Use the correct variable name
+
+		setTimeout(() => {
+			pointText = ""; // Clear the text after 2 seconds
+		}, 2000);
+
+		ball.remove();
+		score += 1;
+	}
+}
+
+
+function healthbar(ball, floor) {
+	let damageText = "-1 health";
 	let textX = ball.x;
 	let textY = ball.y - 20; // Display the text just above the ball's position
-  
-	fill('green');
-	text(pointText, textX, textY); // Use the correct variable name
-  
+
+	fill('red');
+	text(damageText, textX, textY);
+
 	setTimeout(() => {
-	  pointText = ""; // Clear the text after 2 seconds
+		damageText = ""; // Clear the text after 2 seconds
 	}, 2000);
-  
+
 	ball.remove();
-	score += 1;
-  }
-  
-  
-function healthbar(ball, floor) {
-  let damageText = "-1 health";
-  let textX = ball.x;
-  let textY = ball.y - 20; // Display the text just above the ball's position
-
-  fill('red');
-  text(damageText, textX, textY);
-
-  setTimeout(() => {
-    damageText = ""; // Clear the text after 2 seconds
-  }, 2000);
-
-  ball.remove();
-  health -= 1;
+	health -= 1;
 }
